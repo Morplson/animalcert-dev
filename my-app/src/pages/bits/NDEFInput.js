df4879c07088acd00f2ded46a3e3b46ada68812c
pow4you <42853384+Morplson@users.noreply.github.com>
2023-06-26 10:00:06 +0200

    n

--- my-app/src/pages/bits/NDEFInput.js
+++ my-app/src/pages/bits/NDEFInput.js
@@ -3,32 +3,41 @@ import React, { useState, useEffect } from 'react';
 
 const NDEFInput = ({ output }) => {
     const [tagData, setTagData] = useState("");
+    const [isCompatible, setIsCompatible] = useState('NDEFReader' in window);
 
     const handleScan = async () => {
         console.log("User clicked scan button");
-
-        try {
-            //const ndef = new NDEFReader() ;
-            await ndef.scan();
-            console.log("> Scan started");
-
-            ndef.addEventListener("readingerror", () => {
-                console.log("Argh! Cannot read data from the NFC tag. Try another one?");
-            });
-
-            ndef.addEventListener("reading", ({ message, serialNumber }) => {
-                setTagData(`> Serial Number: ${serialNumber}`);
-            });
-        } catch (error) {
-            console.log("Argh! " + error);
+        if (isCompatible) {
+            try {
+                const ndef = new NDEFReader();
+                await ndef.scan();
+                console.log("> Scan started");
+    
+                ndef.addEventListener("readingerror", () => {
+                    console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+                });
+    
+                ndef.addEventListener("reading", ({ message, serialNumber }) => {
+                    setTagData(`> Serial Number: ${serialNumber}`);
+                });
+            } catch (error) {
+                console.log("Argh! " + error);
+            }
         }
+        
     };
 
     return (
-    <div>
-        <button onClick={handleScan}>Read DATA</button>
-        <p>{tagData}</p>
-        </div>
+        <React.Fragment>
+            {isCompatible ?
+                <React.Fragment>
+                    <button className='crypto-button' onClick={handleScan}>Read DATA</button>
+                    <p>{tagData}</p>
+                </React.Fragment>
+                :
+                <div> Browser is not compatible... </div>
+            }
+        </React.Fragment>
     )
 
 }
